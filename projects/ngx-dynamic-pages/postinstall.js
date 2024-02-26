f7fe9d9b62bacc2f9bc7e72ca516e770eaaf6865
carlosengsoft <carlosdaniels.araujo@gmail.com>
2024-02-25 20:52:27 -0400

    t2

--- projects/ngx-dynamic-pages/postinstall.js
+++ projects/ngx-dynamic-pages/postinstall.js
@@ -1,33 +1,18 @@
-const fs = require('fs');
-const path = require('path');
-
-// Seu código para gerar informações
-const preJson = `[
-    {
-        "route": "/home",
-        "components": [
-            {
-                "componentName": "my-custom-button",
-                "inputs": {
-                    "label": "Primary action",
-                    "labelColor": "white",
-                    "backgroundButton": "black"
-                }
-            }
-        ]
-    }
-]`;
-
 // Caminho do arquivo a ser gerado
-//const caminhoArquivo = './src/jsonParameterize.json';
+const caminhoArquivo = './src/jsonParameterize.json';
 
+// Caminho do arquivo do projeto que está instalando a biblioteca
 const caminhoArquivoProjeto = path.resolve(process.cwd(), 'src/app/app.module.ts');
 const importacaoBiblioteca = `import { NgxDynamicPagesModule } from 'ngx-dynamic-pages';`;
 
+// Lê o código do projeto
 const codigoProjeto = fs.readFileSync(caminhoArquivoProjeto, 'utf-8');
+
+// Adiciona a importação ao código do projeto
 const novoCodigoProjeto = codigoProjeto.replace('// Imports de bibliotecas', `${importacaoBiblioteca}\n\n// Imports de bibliotecas`);
 
+// Escreve o novo código no arquivo do projeto
 fs.writeFileSync(caminhoArquivoProjeto, novoCodigoProjeto, 'utf-8');
-//fs.writeFileSync(caminhoArquivo, preJson, 'utf-8');
+fs.writeFileSync(caminhoArquivo, preJson, 'utf-8');
 
-//console.log(`Arquivo ${caminhoArquivo} gerado com sucesso!`);
+console.log(`Arquivo ${caminhoArquivo} gerado com sucesso!`);
